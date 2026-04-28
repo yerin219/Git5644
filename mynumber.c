68bfe5c25f68ffc0278e3ab27973a262e287809d
yerin219 <yerin051219@naver.com>
2026-04-28 12:47:38 +0900

    Add prime numbers output in Testing branch

--- mynumber.c
+++ mynumber.c
@@ -2,17 +2,69 @@
 
 int main(void) {
 
-    for (int i = 1; i <= 100; i += 2) {
+    const int MAX_NUM = 100;
+
+    for (int i = 1; i <= MAX_NUM; i += 2) {
 
         printf("%d ", i);
 
     }
 
     printf("\n");
-for (int i = 2; i <= 100; i += 2) {
+
+
+for (int i = 2; i <= MAX_NUM; i += 2) {
+
         printf("%d ", i);
+
     }
+
     printf("\n");
-    return 0;
+
+
+    for (int num = 2; num <= MAX_NUM; num++) {
+
+        int isPrime = 1; 
+
+        for (int j = 2; j * j <= num; j++) {
+
+            if (num % j == 0) {
+
+                isPrime = 0;
+
+                break;
+
+            }
+
+        }
+
+        if (isPrime) {
+
+            printf("%d ", num);
+
+        }
+
+    }
+
+    printf("\n");
+
+return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
